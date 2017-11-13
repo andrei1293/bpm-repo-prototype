7eb47a9fe146f8f9b08a952316b8bcd6a3443808
Andrii Kopp <kopp93@gmail.com>
2017-11-13 14:03:29 +0200

    Add role and grant features.

--- script/store.js
+++ script/store.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     $('#storeFormError').hide();
     $('#storeFormSubmit').hide();
     $('#afterStoring').hide();
+    $('#grantUserError').hide();
+    $('#grantRolesSection').hide();
 
     $('#newProcess').click(function() {
         $('#existingProcessSection').hide();
@@ -29,6 +31,10 @@ $(document).ready(function() {
 
         if (!$('#modelFile').val()) isStoreFormValid = false;
 
+        if (!$("#openModel").prop("checked") && !$("#grantAccess").prop("checked")) {
+            isStoreFormValid = false;
+        }
+
         if (isStoreFormValid) {
             $('#storeFormError').hide();
             $('#storeModelForm').hide();
@@ -36,6 +42,68 @@ $(document).ready(function() {
             $('#afterStoring').show();
         } else {
             $('#storeFormError').show();
+        }
+    });
+
+    $('#openModel').click(function() {
+        $('#grantRolesSection').hide();
+    });
+
+    $('#grantAccess').click(function() {
+        $('#grantRolesSection').show();
+    });
+
+    $('#addUserReadModel').click(function() {
+        var userName = $('#grantUserField').val();
+        var isValid = true;
+        if (!userName) isValid = false;
+
+        if (grantedUsers.indexOf(userName + '-r') == -1) {
+            if (isValid) {
+                $.get('https://api.github.com/search/users?q=' + userName, function(data) {
+                    if (data.total_count == 1) {
+                        $('#grantUserError').hide();
+                        grantedUsers.push(userName + '-r');
+                        $('#grantUsersList').append('<a class="btn btn-default btn-xs" onclick="var index = grantedUsers.indexOf(\'' +
+                            userName + '-r\'); grantedUsers.splice(index, 1); this.parentNode.removeChild(this);">' +
+                            userName + ' &#9747</a> ');
+                        $('#grantUserField').val('');
+                    } else {
+                        $('#grantUserError').show();
+                    }
+                });
+            } else {
+                $('#grantUserError').show();
+            }
+        } else {
+            $('#grantUserError').hide();
+        }
+    });
+
+    $('#addUserWriteModel').click(function() {
+        var userName = $('#grantUserField').val();
+        var isValid = true;
+        if (!userName) isValid = false;
+
+        if (grantedUsers.indexOf(userName + '-w') == -1) {
+            if (isValid) {
+                $.get('https://api.github.com/search/users?q=' + userName, function(data) {
+                    if (data.total_count == 1) {
+                        $('#grantUserError').hide();
+                        grantedUsers.push(userName + '-w');
+                        $('#grantUsersList').append('<a class="btn btn-primary btn-xs" onclick="var index = grantedUsers.indexOf(\'' +
+                            userName + '-w\'); grantedUsers.splice(index, 1); this.parentNode.removeChild(this);">' +
+                            userName + ' &#9747</a> ');
+                        $('#grantUserField').val('');
+                    } else {
+                        $('#grantUserError').show();
+                    }
+                });
+            } else {
+                $('#grantUserError').show();
+            }
+        } else {
+            $('#grantUserError').hide();
         }
     });
 });
